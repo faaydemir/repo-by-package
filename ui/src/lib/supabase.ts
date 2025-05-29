a4aeefd6adcb1a4a4d4f13016ac8112ad146f46c
fatih <faaydemir@outlook.com>
2025-05-29 11:12:46 +0300

    add fetch-retry

--- ui/src/lib/supabase.ts
+++ ui/src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
+import fetchRetry from 'fetch-retry';
+// Wrap the global fetch with fetch-retry
+const fetchWithRetry = fetchRetry(fetch, { retries: 2 });
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
 	throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
@@ -11,4 +14,9 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
 export const supabase = createClient<Database>(
 	process.env.NEXT_PUBLIC_SUPABASE_URL,
 	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	{
+		global: {
+			fetch: fetchWithRetry,
+		},
+	},
 );
